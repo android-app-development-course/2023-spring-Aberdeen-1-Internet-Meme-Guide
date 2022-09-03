9e23ce58b41cd29c16cffa3f42d7636f1852f43a
Eason_R <rys582132403@163.com>
2022-09-03 16:34:26 +0800

    add bottom navigation bar

--- app/src/main/java/com/example/forumapp/MainActivity.kt
+++ app/src/main/java/com/example/forumapp/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.forumapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        NavigationBarView.OnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.newsNaviBtn -> {
+                    val intent= Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.passageNaviBtn -> {
+                    // TODO: 加路由
+                    true
+                }
+                R.id.postNaviBtn -> {
+                    // TODO: 加路由
+                    true
+                }
+                R.id.myNaviBtn -> {
+                    // TODO: 加路由
+                    true
+                }
+                else -> false
+            }
+        }
+
+        val postBtn=findViewById<FloatingActionButton>(R.id.postNaviBtn)
+        postBtn.setOnClickListener{
+            // TODO
+        }
     }
 }
