c383dbbb011ef16c6a68a4045a3a6fb7fa16bde5
Eason_R <rys582132403@163.com>
2022-09-04 20:41:04 +0800

    add news demonstration

---
+++ app/src/main/java/com/example/forumapp/NewsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.forumapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+
+    }
+}
