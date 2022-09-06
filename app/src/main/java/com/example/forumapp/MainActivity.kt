4ed4466d24df9522cd822e97d23611f652099a6c
Eason_R <rys582132403@163.com>
2022-09-06 13:09:12 +0800

    modified bottomNavigation

--- app/src/main/java/com/example/forumapp/MainActivity.kt
+++ app/src/main/java/com/example/forumapp/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.forumapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationBarView
+import org.xutils.view.annotation.ContentView
+import org.xutils.view.annotation.Event
 
+@ContentView(R.layout.activity_main)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         NavigationBarView.OnItemSelectedListener { item ->
             when(item.itemId){
@@ -38,5 +42,10 @@ class MainActivity : AppCompatActivity() {
         postBtn.setOnClickListener{
             // TODO
         }
+
+//        @Event(R.id.postNaviBtn)
+//        fun onPlusBtnClicked(view: View){
+//
+//        }
     }
 }
