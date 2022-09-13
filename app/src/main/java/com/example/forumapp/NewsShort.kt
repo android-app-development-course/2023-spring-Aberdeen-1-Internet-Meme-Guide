0f96ee7ecc5e1ed7e382c2981ad2c794fccff71c
Eason_R <rys582132403@163.com>
2022-09-13 16:04:36 +0800

    transfer newsActivity -> fragment

--- app/src/main/java/com/example/forumapp/NewsShort.kt
+++
@@ -1,8 +0,0 @@
-package com.example.forumapp
-
-import android.accounts.Account
-import java.sql.Time
-
-class NewsShort (val title:String, val pictureId:Int, val time:Time, val author:Account)
-
-//以后再研究Time，Account，pictureId的用法
--- app/src/main/java/com/example/forumapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/forumapp/activity/MainActivity.kt
@@ -1,23 +1,22 @@
-package com.example.forumapp
+package com.example.forumapp.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.view.Window
-import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
+import com.example.forumapp.fragment.NewsFragment
+import com.example.forumapp.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationBarView
-import org.xutils.view.annotation.ContentView
-import org.xutils.view.annotation.Event
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)  // 放在setContentView之前
         setContentView(R.layout.activity_main)
-        replaceMainContent(NewsFragment())
+        replaceMainContent(NewsFragment())  // 默认打开新闻页
+
+        // 新闻处理
 
 
         NavigationBarView.OnItemSelectedListener { item ->
@@ -28,6 +27,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.passageNaviBtn -> {
                     // TODO: 加路由
+
                     true
                 }
                 R.id.postNaviBtn -> {
