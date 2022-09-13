da8ee73b64c6841823e0f9f9b0a23113ef8d3510
Xiong <1723572491@qq.com>
2022-09-13 16:46:17 +0800

    搞了fragment和recycler

--- app/src/main/java/com/example/forumapp/NewsUnit.kt
+++ app/src/main/java/com/example/forumapp/NewsUnit.kt
@@ -1,32 +1,32 @@
-package com.example.forumapp
-
-import android.app.Activity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.TextView
-
-class NewsUnit(val title:String,val imageId:Int, val author:String, val time:String)
-
-//适配器
-class NewsAdapter(activity: Activity, val resourceId: Int, data: List<NewsUnit>) :
-    ArrayAdapter<NewsUnit>(activity, resourceId, data) {
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = LayoutInflater.from(context).inflate(resourceId, parent, false)
-        val newsImage: ImageView = view.findViewById(R.id.newsPicture)
-        val newsTitle: TextView = view.findViewById(R.id.newsTitle)
-        val newsTime: TextView = view.findViewById(R.id.newsTime)
-        val newsAuthor: TextView = view.findViewById(R.id.newsAuthor)
-        val news = getItem(position) // 获取当前项的news实例
-        if (news != null) {
-            newsImage.setImageResource(news.imageId)
-            newsTitle.text = news.title
-            newsTime.text = news.time
-            newsAuthor.text = news.author
-
-        }
-        return view
-    }
-}
+//package com.example.forumapp
+//
+//import android.app.Activity
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.ArrayAdapter
+//import android.widget.ImageView
+//import android.widget.TextView
+//
+//class NewsUnit(val title:String,val imageId:Int, val author:String, val time:String)
+//
+////适配器
+//class NewsAdapter(activity: Activity, val resourceId: Int, data: List<NewsUnit>) :
+//    ArrayAdapter<NewsUnit>(activity, resourceId, data) {
+//    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+//        val view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+//        val newsImage: ImageView = view.findViewById(R.id.newsPicture)
+//        val newsTitle: TextView = view.findViewById(R.id.newsTitle)
+//        val newsTime: TextView = view.findViewById(R.id.newsTime)
+//        val newsAuthor: TextView = view.findViewById(R.id.newsAuthor)
+//        val news = getItem(position) // 获取当前项的news实例
+//        if (news != null) {
+//            newsImage.setImageResource(news.imageId)
+//            newsTitle.text = news.title
+//            newsTime.text = news.time
+//            newsAuthor.text = news.author
+//
+//        }
+//        return view
+//    }
+//}
