2a5531cbd62eb543b1a5aaee5562313b51121f2f
Eason_R <rys582132403@163.com>
2022-11-08 10:09:06 +0800

    修复依赖冲突

--- app/src/main/java/com/example/forumapp/fragment/ArticleFragment.kt
+++ app/src/main/java/com/example/forumapp/fragment/ArticleFragment.kt
@@ -7,15 +7,10 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import com.example.forumapp.R
+import com.rex.editor.view.RichEditorNew
 
-class fragment_article : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-    }
+class ArticleFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +25,38 @@ class fragment_article : Fragment() {
         }
 
         // Inflate the layout for this fragment
+
+
+        val richTextContent = view.findViewById<RichEditorNew>(R.id.richText)
+        val htmlTest = "<!DOCTYPE html>" +
+                "<html>" +
+                "    <head>" +
+                "        <meta charset=\"utf-8\">" +
+                "        <title>主页 - Eason_R</title>" +
+                "        <meta name=\"creater\" content=\"Eason_R\">" +
+                "    </head>" +
+                "    <body>" +
+                "        <h1>欢迎来到主页</h1>" +
+                "        <p>点击下列链接即可跳转</p>" +
+                "        <ul>" +
+                "            <li>主页</li>" +
+                "            <li>跳转测试 - <a href=\"\">论坛</a></li>" +
+                "            <li><a href=\"\">gangsters</a></li>" +
+                "            <li><a href=\"\">美味宫保鸡丁食谱★</a></li>" +
+                "            <li><a href=\"\">JS测试</a></li>" +
+                "        </ul>" +
+                "    </body>" +
+                "</html>"
+
+//        // 检测是否拥有权限
+//        if(ContextCompat.checkSelfPermission(this.requireContext(),
+//                android.Manifest.permission.READ_EXTERNAL_STORAGE)
+//            != PackageManager.PERMISSION_GRANTED){
+//
+//        }
+
+        richTextContent.loadRichEditorCode(htmlTest)
+
         return view
     }
 
