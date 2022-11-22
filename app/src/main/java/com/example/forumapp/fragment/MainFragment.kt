69fa6b482db4916939ba4a65f1fd3c04620b5fdc
Xiong <1723572491@qq.com>
2022-11-22 15:55:08 +0800

    .

--- app/src/main/java/com/example/forumapp/fragment/MainFragment.kt
+++ app/src/main/java/com/example/forumapp/fragment/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.forumapp.fragment
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.forumapp.ArticleEditActivity
 import com.example.forumapp.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -15,6 +17,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class MainFragment:NavHostFragment() {
 
 
+    @SuppressLint("ResourceType")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -41,6 +44,7 @@ class MainFragment:NavHostFragment() {
                 }
                 R.id.myNaviBtn -> {
                     // TODO: 加路由
+                    findNavController().navigate(R.navigation.nav_graph)
                     true
                 }
                 else -> false
