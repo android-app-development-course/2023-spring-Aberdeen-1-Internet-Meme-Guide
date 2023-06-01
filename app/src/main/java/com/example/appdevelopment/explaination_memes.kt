7f2ec02f3df9674a86c05dcda9992dea25758aff
Bilibili <bilibili.com>
2023-06-01 22:19:34 +0800

    更新了探索页

--- app/src/main/java/com/example/appdevelopment/explaination_memes.kt
+++ app/src/main/java/com/example/appdevelopment/explaination_memes.kt
@@ -7,31 +7,31 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class explaination_memes : AppCompatActivity() {
-    private lateinit var tvPanelContent: TextView
-    private lateinit var imgExpand: ImageView
-    private var isExpanded = false
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.explaination_memes)
-
-         tvPanelContent = findViewById(R.id.tv_panel_content)
-         imgExpand = findViewById(R.id.img_expand)
-
-        imgExpand.setOnClickListener {
-            isExpanded = !isExpanded
-
-            if (isExpanded) {
-                tvPanelContent.maxLines = Integer.MAX_VALUE
-                tvPanelContent.layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
-                imgExpand.setImageResource(R.drawable.ic_arrow_up)
-            } else {
-                tvPanelContent.maxLines = 10
-                tvPanelContent.layoutParams.height = resources.getDimensionPixelSize(R.dimen.text_height)
-                imgExpand.setImageResource(R.drawable.ic_arrow_down)
-            }
-
-        }
-    }
+//    private lateinit var tvPanelContent: TextView
+//    private lateinit var imgExpand: ImageView
+//    private var isExpanded = false
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.explaination_memes)
+//
+//         tvPanelContent = findViewById(R.id.tv_panel_content)
+//         imgExpand = findViewById(R.id.img_expand)
+//
+//        imgExpand.setOnClickListener {
+//            isExpanded = !isExpanded
+//
+//            if (isExpanded) {
+//                tvPanelContent.maxLines = Integer.MAX_VALUE
+//                tvPanelContent.layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
+//                imgExpand.setImageResource(R.drawable.ic_arrow_up)
+//            } else {
+//                tvPanelContent.maxLines = 10
+//                tvPanelContent.layoutParams.height = resources.getDimensionPixelSize(R.dimen.text_height)
+//                imgExpand.setImageResource(R.drawable.ic_arrow_down)
+//            }
+//
+//        }
+//    }
 
 }
