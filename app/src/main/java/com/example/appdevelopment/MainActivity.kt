bc6b1d77ffc7bf5b27ea92aa3656490a6f897476
NagiicivA <1292095597@qq.com>
2023-06-04 18:32:36 +0800

    test

--- app/src/main/java/com/example/appdevelopment/MainActivity.kt
+++ app/src/main/java/com/example/appdevelopment/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.appdevelopment
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    val yysy = findViewById<TextView>(R.id.YYSY)
-        yysy.setOnClickListener{
+        val yysy = findViewById<TextView>(R.id.YYSY)
+        yysy.setOnClickListener {
             val intent = Intent(this, explaination_memes::class.java)
             startActivity(intent)
         }
+
     }
 }
+
+
