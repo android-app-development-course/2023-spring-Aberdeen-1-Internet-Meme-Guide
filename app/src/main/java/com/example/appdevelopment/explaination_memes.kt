a28750e2175debb833b6c08678108d8ebf2716b9
NagiicivA <1292095597@qq.com>
2023-06-06 00:39:07 +0800

    explaination_memes

--- app/src/main/java/com/example/appdevelopment/explaination_memes.kt
+++ app/src/main/java/com/example/appdevelopment/explaination_memes.kt
@@ -6,6 +6,8 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class explaination_memes : AppCompatActivity() {
@@ -14,6 +16,17 @@ class explaination_memes : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.explaination_memes)
         val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        val explainationMemesList = listOf(
+            ExplainationMemes(1, R.drawable.pic2, "泰裤辣", "34万浏览",R.drawable.pic3, "只因你太美", "27万浏览"),
+            ExplainationMemes(2, R.drawable.powerman, "大力王", "29万浏览", R.drawable.xiaodaidai, "小呆呆", "22万浏览"),
+            ExplainationMemes(3, R.drawable.wujing, "吴京", "15万浏览", R.drawable.wujing, "YYSY", "235万浏览"),
+            // 可以添加更多数据
+        )
+
+        val recyclerView = findViewById<RecyclerView>(R.id.explaination_rv)
+        recyclerView.adapter = ExplainationMemesAdapter(explainationMemesList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
 
         navView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
