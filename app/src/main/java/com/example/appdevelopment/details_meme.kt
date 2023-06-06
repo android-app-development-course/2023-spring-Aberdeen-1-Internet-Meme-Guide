7e08e8a9bda2498cff3b1a17787852e9d6112d81
Bilibili <bilibili.com>
2023-06-07 00:48:20 +0800

    主页布局修改调整完成

--- app/src/main/java/com/example/appdevelopment/details_meme.kt
+++ app/src/main/java/com/example/appdevelopment/details_meme.kt
@@ -2,6 +2,7 @@ package com.example.appdevelopment
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -9,6 +10,12 @@ class details_meme: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.details_meme)
+        val backtn = findViewById<ImageView>(R.id.imageView7)
+
+        backtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
