eba9cb333fa06ada0d96f53570541a2443fa02a4
NagiicivA <1292095597@qq.com>
2023-06-07 20:18:05 +0800

    发布页假数据

--- app/src/main/java/com/example/appdevelopment/CollapsePanel.kt
+++ app/src/main/java/com/example/appdevelopment/CollapsePanel.kt
@@ -1,5 +1,8 @@
 package com.example.appdevelopment
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class CollapsePanel(
     val id: Int,
     val tittle: String,
@@ -7,6 +10,34 @@ data class CollapsePanel(
     val likes: String,
     val time: String,
     var isExpanded: Boolean = false
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString()
+    )
 
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(tittle)
+        parcel.writeString(content)
+        parcel.writeString(likes)
+        parcel.writeString(time)
+    }
 
-)
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<CollapsePanel> {
+        override fun createFromParcel(parcel: Parcel): CollapsePanel {
+            return CollapsePanel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<CollapsePanel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/example/appdevelopment/deliver.kt
+++ app/src/main/java/com/example/appdevelopment/deliver.kt
@@ -3,18 +3,33 @@ package com.example.appdevelopment
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class deliver : AppCompatActivity() {
+    private val collapsePanels = mutableListOf<CollapsePanel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.deliver)
         val exitbtn = findViewById<ImageView>(R.id.exitbtn)
+        val sendbtn = findViewById<TextView>(R.id.sendbtn)
+
 
         exitbtn.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        sendbtn.setOnClickListener {
+            val newPanel = CollapsePanel(6, "NSDD","现如今，很多人开始用拼音首字母来代替自己想要表达的意思，这是一种在零零后群体中比较流行的字母缩写流，例如“啊我死了”被说成“awsl”", "14", "June 6")
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("newPanel", newPanel)
+            startActivity(intent)
+        }
+
 
     }
 }
